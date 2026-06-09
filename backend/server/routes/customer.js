8894bf7e34ad6c4f37477a24fbb7b4f311877bc4
oshokedaniel <oshokedaniel08@gmail.com>
2026-06-09 15:59:38 +0100

    safer google private key

--- backend/server/routes/customer.js
+++ backend/server/routes/customer.js
@@ -8,7 +8,12 @@ function getAuth() {
   return new google.auth.GoogleAuth({
     credentials: {
       client_email: process.env.GOOGLE_CLIENT_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      private_key: process.env.GOOGLE_PRIVATE_KEY
+        ? process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n").replace(
+            /\r/g,
+            "",
+          )
+        : undefined,
     },
     scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
   });
